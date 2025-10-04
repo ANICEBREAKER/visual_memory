ab9477bf65fe50b083cf05d4befe10bb2c1e6773
ANICEBREAKER <vnquanganh22112009@gmail.com>
2025-10-04 21:20:13 +0700

    - Added the memory files from the flutter_game_project, it stored the game difficulty, but still had some bugs [Including having the same high score for all 3 difficulties]

---
+++ lib/player_progress/persistence/local_storage_player_progress_persistence.dart
@@ -0,0 +1,31 @@
+// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'player_progress_persistence.dart';
+
+/// An implementation of [PlayerProgressPersistence] that uses
+/// `package:shared_preferences`.
+class LocalStoragePlayerProgressPersistence extends PlayerProgressPersistence {
+  final Future<SharedPreferences> instanceFuture =
+      SharedPreferences.getInstance();
+
+  @override
+  Future<int> getHighestLevelReached({difficulty = " ", game = ""}) async {
+    final prefs = await instanceFuture;
+    String infoDestination = 'highestLevelReached_${game}_$difficulty';
+    print("Getting highest level for Difficulty: $difficulty, Game: $game");
+    int highestLevelReached = prefs.getInt(infoDestination) ?? 0;
+    print(highestLevelReached);
+    return highestLevelReached;
+  }
+
+  @override
+  Future<void> saveHighestLevelReached({level = 0, difficulty = " ", game = ""}) async {
+    final prefs = await instanceFuture;
+    String infoDestination = 'highestLevelReached_${game}_$difficulty';
+    print("Saving highest level for Difficulty: $difficulty, Game: $game, Level: $level");
+    await prefs.setInt(infoDestination, level);
+  }
+}
---
+++ lib/player_progress/player_progress.dart
@@ -0,0 +1,62 @@
+// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'persistence/local_storage_player_progress_persistence.dart';
+import 'persistence/player_progress_persistence.dart';
+
+/// Encapsulates the player's progress.
+class PlayerProgress extends ChangeNotifier {
+
+  /// By default, settings are persisted using
+  /// [LocalStoragePlayerProgressPersistence] (i.e. NSUserDefaults on iOS,
+  /// SharedPreferences on Android or local storage on the web).
+  final PlayerProgressPersistence _store;
+
+  int _highestLevelReached = 0;
+  bool isNewHighScore = false;
+
+  /// Creates an instance of [PlayerProgress] backed by an injected
+  /// persistence [store].
+  PlayerProgress({PlayerProgressPersistence? store})
+    : _store = store ?? LocalStoragePlayerProgressPersistence();
+
+  /// The highest level that the player has reached so far.
+  int get highestLevelReached => _highestLevelReached;
+
+  /// Resets the player's progress so it's like if they just started
+  /// playing the game for the first time.
+  void reset(String game, String difficulty) {
+    _highestLevelReached = 0;
+    notifyListeners();
+    _store.saveHighestLevelReached(level: _highestLevelReached, game: game, difficulty: difficulty);
+  }
+
+  /// Registers [level] as reached.
+  ///
+  /// If this is higher than [highestLevelReached], it will update that
+  /// value and save it to the injected persistence store.
+  void setLevelReached(int level, String game, String difficulty) {
+    if (level > _highestLevelReached) {
+      _highestLevelReached = level;
+      isNewHighScore = true;
+      notifyListeners();
+      unawaited(_store.saveHighestLevelReached(level: level, game: game, difficulty: difficulty));
+    } else {
+      isNewHighScore = false;
+    }
+  }
+
+  /// Fetches the latest data from the backing persistence store.
+  Future<void> getLatestFromStore(String game, String difficulty) async {
+    final level = await _store.getHighestLevelReached(game: game, difficulty: difficulty);
+    if (level > _highestLevelReached) {
+      _highestLevelReached = level;
+      notifyListeners();
+    } else if (level < _highestLevelReached) {
+      await _store.saveHighestLevelReached(level: _highestLevelReached, game: game, difficulty: difficulty);
+    }
+  }
+}
