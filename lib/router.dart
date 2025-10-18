aaa32f08f705c35a683e4ed195af97f17e7dc3c6
ANICEBREAKER <vnquanganh22112009@gmail.com>
2025-10-18 20:17:59 +0700

    Added some new routes from the login/register/welcome screen

--- lib/router.dart
+++ lib/router.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:game_testing/screens/common/login_screen.dart';
+import 'package:game_testing/screens/common/registration_screen.dart';
+import 'package:game_testing/screens/common/welcome_screen.dart';
 import 'package:game_testing/screens/visual_memory/visual_memory_logic/level_state.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'screens/visual_memory/visual_memory_game_screen.dart';
 import 'screens/visual_memory/visual_memory_result_screen.dart';
 import 'screens/visual_memory/visual_memory_start_screen.dart';
+import 'screens/common/menu_screen.dart';
 
 final GoRouter visualMemoryGoRouter = GoRouter(
   routes: <RouteBase>[
 GoRoute(
 path: '/',
   builder: (BuildContext context, GoRouterState state) {
-    return const VisualMemoryStartScreen();
+    return WelcomeScreen();
   },
   routes: <RouteBase>[
     GoRoute(
@@ -34,5 +38,8 @@ path: '/',
         return VisualMemoryResultScreen(level: level, difficulty: state.uri.queryParameters['difficulty'].toString());
       },
     ),
+    GoRoute(path: '/login', builder: (BuildContext context, GoRouterState state) => LoginScreen()),
+    GoRoute(path: '/register', builder: (BuildContext context, GoRouterState state) => RegistrationScreen()),
+    GoRoute(path: '/menu', builder: (BuildContext context, GoRouterState state) => MenuScreen()),
   ],
 )]);
