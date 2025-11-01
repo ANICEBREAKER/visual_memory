d7584e8bf10791cb9d05e02b89c0f8bf03161e4a
ANICEBREAKER <vnquanganh22112009@gmail.com>
2025-11-01 21:17:37 +0700

    Added the template for the quick_maths_game_screen.dart

---
+++ lib/screens/quick_maths/quick_maths_game_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuickMathsGameScreen extends StatefulWidget {
+  const QuickMathsGameScreen({super.key});
+
+  @override
+  State<QuickMathsGameScreen> createState() => _QuickMathsGameScreenState();
+}
+
+class _QuickMathsGameScreenState extends State<QuickMathsGameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
