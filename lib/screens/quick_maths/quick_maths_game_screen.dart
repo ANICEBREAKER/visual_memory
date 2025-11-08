c06d88b029455b6898992e7d65005c0a35d11894
ANICEBREAKER <vnquanganh22112009@gmail.com>
2025-11-08 20:56:46 +0700

    Made the general UI for the game and added a numpad to the bottom of the screen

--- lib/screens/quick_maths/quick_maths_game_screen.dart
+++ lib/screens/quick_maths/quick_maths_game_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../router.dart';
+import '../quick_maths/widget/simple_numpad.dart';
 
 class QuickMathsGameScreen extends StatefulWidget {
   const QuickMathsGameScreen({super.key});
@@ -10,6 +13,117 @@ class QuickMathsGameScreen extends StatefulWidget {
 class _QuickMathsGameScreenState extends State<QuickMathsGameScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final mediaQuery = MediaQuery.of(context);
+    final screenWidth = mediaQuery.size.width;
+    final screenHeight = mediaQuery.size.height;
+
+    // // Responsive paddings and spacings
+    // final horizontalPadding = screenWidth * 0.07; // 7% of width
+    // final gridSpacing = screenWidth * 0.010; // ~2% of width
+    // final gridPadding = screenHeight * 0.015; // ~1.5% of height
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              context.go(RoutePath.menu.path);
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            )),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(screenWidth * 0.02), // 2% of width
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text("Levels: ",
+                        style: Theme.of(context).textTheme.labelSmall),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text("Lives: 🖤🖤🖤")
+                    // Text(
+                    //     context.watch<VisualMemoryLevelState>().lives == 3
+                    //         ? "Lives: 🖤🖤🖤"
+                    //         : context.watch<VisualMemoryLevelState>().lives == 2
+                    //         ? "Lives: 🖤🖤🤍"
+                    //         : context.watch<VisualMemoryLevelState>().lives == 1
+                    //         ? "Lives: 🖤🤍🤍"
+                    //         : "Lives: 🤍🤍🤍",
+                    //     style: Theme.of(context).textTheme.labelSmall
+                    // ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.01),
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: Colors.grey[300],
+              ),
+            ), //Displaying equations
+            SizedBox(height: screenHeight * 0.01),
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.grey[600],
+                width: double.infinity,
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: SimpleNumpad(
+                    buttonWidth: 40,
+                    buttonHeight: screenHeight * 0.022, // TODO: Make responsive height for buttons
+                    gridSpacing: 5,
+                    buttonBorderRadius: 5,
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.black.withAlpha(200),
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    useBackspace: true,
+                    optionText: 'Clear',
+                    onPressed: (str) {
+                      print(str);
+                      //Insert logic/function to handle numpad input
+                    },
+                  ),
+                ),
+            )
+            ) //At the bottom of the screen showing numpad
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/quick_maths/widget/simple_numpad.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+/// A simple numpad.
+class SimpleNumpad extends StatelessWidget {
+  const SimpleNumpad({
+    super.key,
+    required this.buttonWidth,
+    required this.buttonHeight,
+    this.gridSpacing = 2,
+    required this.onPressed,
+    this.optionText,
+    this.textStyle = const TextStyle(
+      color: Colors.black,
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+    ),
+    this.foregroundColor = Colors.black,
+    this.backgroundColor = Colors.white,
+    this.buttonBorderSide = BorderSide.none,
+    this.buttonBorderRadius = 0,
+    this.useBackspace = false,
+    this.backspaceSize = 24,
+    this.removeBlankButton = false,
+  });
+
+  /// Button width.
+  final double buttonWidth;
+
+  /// Button height.
+  final double buttonHeight;
+
+  /// Spacing of buttons.
+  final double gridSpacing;
+
+  /// Button event handle function.
+  final Function(String) onPressed;
+
+  /// Bottom left button text.
+  final String? optionText;
+
+  /// Text style.
+  final TextStyle textStyle;
+
+  /// Button foreground color.
+  final Color foregroundColor;
+
+  /// Button background color.
+  final Color backgroundColor;
+
+  /// Button border side.
+  final BorderSide buttonBorderSide;
+
+  /// Button border radius.
+  final double buttonBorderRadius;
+
+  /// Use bottom right backspace option.
+  final bool useBackspace;
+
+  /// Bottom right backspace
+  final double backspaceSize;
+
+  /// Remove blank button option.
+  /// If you turn on this option,
+  /// Deactivated bottom left and right buttons buttons will be removed.
+  final bool removeBlankButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: buttonWidth * 3 + gridSpacing * 2,
+      child: GridView.count(
+        padding: EdgeInsets.zero,
+        physics: const NeverScrollableScrollPhysics(),
+        crossAxisCount: 3,
+        crossAxisSpacing: gridSpacing,
+        mainAxisSpacing: gridSpacing,
+        shrinkWrap: true,
+        childAspectRatio: buttonWidth / buttonHeight,
+        children: [
+          _padTextButton("1"),
+          _padTextButton("2"),
+          _padTextButton("3"),
+          _padTextButton("4"),
+          _padTextButton("5"),
+          _padTextButton("6"),
+          _padTextButton("7"),
+          _padTextButton("8"),
+          _padTextButton("9"),
+          optionText != null ? _padTextButton(optionText!) : _padDummyButton(),
+          _padTextButton("0"),
+          useBackspace ? _padImageButton() : _padDummyButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _flatButton(
+      Widget child,
+      VoidCallback onPressed,
+      ) {
+    return ElevatedButton(
+      clipBehavior: Clip.antiAlias,
+      style: OutlinedButton.styleFrom(
+        fixedSize: const Size(
+          double.maxFinite,
+          double.maxFinite,
+        ),
+        padding: EdgeInsets.zero,
+        alignment: Alignment.center,
+        foregroundColor: foregroundColor,
+        backgroundColor: backgroundColor,
+        side: buttonBorderSide,
+        splashFactory: NoSplash.splashFactory,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            buttonBorderRadius,
+          ),
+        ),
+      ),
+      onPressed: onPressed,
+      child: child,
+    );
+  }
+
+  Widget _padDummyButton() {
+    if (removeBlankButton) {
+      return const SizedBox.shrink();
+    }
+
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(buttonBorderRadius),
+        border: Border.all(
+          color: buttonBorderSide.color,
+          strokeAlign: buttonBorderSide.strokeAlign,
+          style: buttonBorderSide.style,
+          width: buttonBorderSide.width,
+        ),
+      ),
+    );
+  }
+
+  Widget _padTextButton(String text) {
+    return _flatButton(
+      Text(
+        text,
+        style: textStyle,
+      ),
+          () => onPressed(text),
+    );
+  }
+
+  Widget _padImageButton() {
+    return _flatButton(
+      _backspaceSvg(),
+          () => onPressed('BACKSPACE'),
+    );
+  }
+
+  Widget _backspaceSvg() {
+    return Icon(Icons.backspace_outlined);
+  }
+}
