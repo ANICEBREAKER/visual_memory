bd9bbe45ceaf5bef9f559b406ef8823abe4353ad
ANICEBREAKER <vnquanganh22112009@gmail.com>
2026-03-14 18:11:06 +0700

    - Added new design for the game tiles - Added new colors for each tile in the app_colors

--- lib/data/game_list.dart
+++ lib/data/game_list.dart
@@ -1,51 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:game_testing/screens/visual_memory/visual_memory_start_screen.dart';
 import 'package:game_testing/screens/quick_maths/quick_maths_start_screen.dart';
+import '../../theme/app_colors.dart';
+
 // The class for a game item
 class GameTileItem {
   final String name;
   final String description;
-  final String icon; // Link from online (SVG or PNG)
+  final IconData icon;
   final Widget destination;
+  final Color? iconColor;
+  final Color? displayColor;
 
   GameTileItem({
     required this.name,
     required this.description,
     required this.icon,
     this.destination = const Placeholder(), // Default destination
+    required this.iconColor,
+    required this.displayColor,
   });
 }
-
 
 // Dummy list of games
 List<GameTileItem> dummyGames = [
   // --- Games from your image ---
   GameTileItem(
-    name: "Visual memory",
-    description: "Memorize patterns on a grid of squares",
-    icon: "https://static.thenounproject.com/png/4411488-200.png", // A brain/memory icon
-    destination: VisualMemoryStartScreen(),
-  ),
+      name: "Visual memory",
+      description: "Memorize patterns on a grid of squares",
+      icon: Icons.grid_view_rounded,
+      // A brain/memory icon
+      destination: VisualMemoryStartScreen(),
+      iconColor: AppColors.iconMemory,
+      displayColor: AppColors.iconMemoryBg),
   GameTileItem(
-    name: "Flashing tiles",
-    description: "Remember the patterns of the tiles flashing",
-    icon: "https://static.thenounproject.com/png/4411488-200.png", // A grid icon
-  ),
+      name: "Flashing tiles",
+      description: "Remember the patterns of the tiles flashing",
+      icon: Icons.bolt_rounded,
+      // A grid icon
+      iconColor: AppColors.iconTiles,
+      displayColor: AppColors.iconTilesBg),
   GameTileItem(
-    name: "Quick maths",
-    description: "Solve as many math equations as fast as you can!",
-    icon: "https://static.thenounproject.com/png/4411488-200.png", // A calculator icon
-    destination: QuickMathsStartScreen(),
-  ),
+      name: "Quick maths",
+      description: "Solve as many math equations as fast as you can!",
+      icon: Icons.calculate_rounded,
+      // A calculator icon
+      destination: QuickMathsStartScreen(),
+      iconColor: AppColors.iconMath,
+      displayColor: AppColors.iconMathBg),
 
   GameTileItem(
-    name: "Word Ladder",
-    description: "Change one letter to form a new word",
-    icon: "https://static.thenounproject.com/png/4411488-200.png", // A ladder icon
-  ),
+      name: "Word Ladder",
+      description: "Change one letter to form a new word",
+      icon: Icons.sort_by_alpha_rounded,
+      // A ladder icon
+      iconColor: AppColors.iconWord,
+      displayColor: AppColors.iconWordBg),
   GameTileItem(
-    name: "Color Match",
-    description: "Match the color to its name against the clock",
-    icon: "https://static.thenounproject.com/png/4411488-200.png", // A color palette icon
-  ),
+      name: "Color Match",
+      description: "Match the color to its name against the clock",
+      icon: Icons.palette_rounded,
+      // A color palette icon
+      iconColor: AppColors.iconColor,
+      displayColor: AppColors.iconColorBg),
+  GameTileItem(
+      name: "Logic Flow",
+      description: "Match the dots!",
+      icon: Icons.psychology_rounded,
+      // A color palette icon
+      iconColor: AppColors.iconLogic,
+      displayColor: AppColors.iconLogicBg),
 ];
--- lib/screens/common/temp_game_screen.dart
+++ lib/screens/common/temp_game_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:game_testing/data/game_list.dart';
+import 'package:go_router/go_router.dart';
+import '../../router.dart';
+import '../../theme/app_colors.dart';
 import '../../theme/responsive_config.dart';
 
 class TempGameScreen extends StatefulWidget {
@@ -13,33 +16,97 @@ class _TempGameScreenState extends State<TempGameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.backgroundDarkDimmed,
       appBar: AppBar(
-        leading: IconButton(onPressed: () {Navigator.pop(context);}, icon: Icon(Icons.arrow_back, color: Colors.white,)),
-        title: Text('Select the game you wanna play', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
+        leading: IconButton(
+            onPressed: () {
+              context.go(RoutePath.menu.path);
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            )),
+        // title: Text(
+        //   'Select the game you wanna play',
+        //   style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        // ),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.settings, color: Colors.white,))
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ))
         ],
       ),
-      body: ListView.builder(
-        itemCount: dummyGames.length,
-          itemBuilder: (context, index) => Padding(
-            padding: ResponsiveConfig.padding(context, size: PaddingSize.xs),
-            child: ListTile(
-              title: Text(dummyGames[index].name, style: Theme.of(context).textTheme.labelMedium,),
-              subtitle: Text(dummyGames[index].description, style: Theme.of(context).textTheme.labelSmall),
-              leading: Icon(Icons.square_rounded, color: Colors.black,),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => dummyGames[index].destination),
-                );
-              },
-              tileColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
+      body: Padding(
+        padding: ResponsiveConfig.padding(context, size: PaddingSize.m),
+        child: Column(children: [
+          Center(
+            child: Text(
+              'Select game',
+              style: TextStyle(
+                fontSize: 45.0,
+                fontWeight: FontWeight.w900,
+                color: AppColors.textPrimaryDark,
+              ),
             ),
-          )
+          ),
+          SizedBox(height: 20.0),
+          Expanded(
+            child: ListView.builder(
+                itemCount: dummyGames.length,
+                itemBuilder: (context, index) => Padding(
+                      padding:
+                          ResponsiveConfig.padding(context, size: PaddingSize.xs),
+                      child: ListTile(
+                        title: Text(
+                          dummyGames[index].name,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w900,
+                            color: AppColors.textPrimaryDark,
+                          ),
+                        ),
+                        subtitle: Text(dummyGames[index].description,
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w900,
+                              color: AppColors.textSecondaryDark,
+                            ),
+                        ),
+                        leading: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: dummyGames[index].displayColor,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Center(
+                              child:
+                                  Icon(dummyGames[index].icon, color: dummyGames[index].iconColor)),
+                        ),
+                        trailing: Icon(Icons.arrow_forward_ios, color: Colors.white),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    dummyGames[index].destination),
+                          );
+                        },
+                        tileColor: AppColors.surfaceDarkVariant,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          side: BorderSide(
+                            color: AppColors.borderDark,
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                    )),
+          ),
+        ]),
       ),
     );
   }
